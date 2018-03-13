9052667872bd51d628232de1794ad080b89bd535
Gert Wollny <gw.fossdev@gmail.com>
2018-03-13 21:35:06 +0100

    Add first tests

---
+++ r600/cf_parsing_test.cpp
@@ -0,0 +1,39 @@
+#include <r600/disassembler.h>
+
+#include <gtest/gtest.h>
+
+using std::vector;
+
+class TestDisassember : public testing::Test {
+protected:
+   void run(const std::vector<uint64_t>& bc, const std::string& expect) const;
+
+};
+
+void TestDisassember::run(const std::vector<uint64_t>& bc,
+                          const std::string& expect) const
+{
+   disassembler diss(bc);
+   ASSERT_EQ(diss.as_string(), expect);
+}
+
+TEST_F(TestDisassember, NopEOP)
+{
+   vector<uint64_t> bc = {
+      end_of_program_bit
+   };
+
+   run(bc, "NOP                    EOP\n");
+}
+
+TEST_F(TestDisassember, NopNopEOP)
+{
+   vector<uint64_t> bc = {
+      0,
+      end_of_program_bit
+   };
+
+   run(bc, "NOP                    \n"
+           "NOP                    EOP\n"
+       );
+}
