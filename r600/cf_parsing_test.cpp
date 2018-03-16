a376e57855d7185fc543761b794482c3603b3978
Gert Wollny <gw.fossdev@gmail.com>
2018-03-16 11:43:34 +0100

    Add test for GDS clause

--- r600/cf_parsing_test.cpp
+++ r600/cf_parsing_test.cpp
@@ -212,5 +212,7 @@ TEST_F(BasicTest, BytecodeCreationNative)
    EXPECT_BITS_EQ(cf_native_spacing, cf_native_node(cf_jump_table, cf_node::barrier, 256, 0, 0, 3).get_bytecode_byte(0),
                   0x8740000003000100ul);
 
+   EXPECT_BITS_EQ(cf_native_spacing, cf_native_node(cf_gds, cf_node::barrier, 256, 0, 3, 0).get_bytecode_byte(0),
+                  0x80C00C0000000100ul);
 
 }
