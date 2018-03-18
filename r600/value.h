ebb5e57d221ca04adc873b7b99ebf58719c109ee
Gert Wollny <gw.fossdev@gmail.com>
2018-03-18 16:03:18 +0100

    Add copyright information

--- r600/value.h
+++ r600/value.h
@@ -1,3 +1,23 @@
+/* -*- mia-c++  -*-
+ *
+ * This file is part of R600-disass a tool to disassemble R600 byte code. 
+ * Copyright (c) Genoa 2018 Gert Wollny
+ *
+ * R600-disass  is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MIA; if not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 #ifndef R600VALUE_H
 #define R600VALUE_H
 
